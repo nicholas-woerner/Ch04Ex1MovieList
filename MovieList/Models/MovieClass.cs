ee09af18a1a813226e52a60c0b96921c51386fe3
Nicholas Woerner <nicholas.woerner@southeasttech.edu>
2023-10-01 17:30:57 -0500

    steps 5-8 because i misread the "6" as "8"

---
+++ MovieList/Models/MovieClass.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieList.Models
+{
+    public class MovieClass
+    {
+       //EF Core configure the database to generate the value
+       public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set;}
+
+        [Required(ErrorMessage = "Please enter a year.")]
+        [Range(1889, 2999, ErrorMessage = "Year must be after 1889.")]
+        public int? Year { get; set; }
+
+        [Required(ErrorMessage = "Please enter a rating.")]
+        [Range(1, 5, ErrorMessage = "Rating must be between 1 and 5.")]
+        public int? Rating { get; set; }
+    }
+}
---
+++ MovieList/Models/MovieContextClass.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieList.Models
+{
+    public class MovieContextClass : DbContext
+    {
+        public MovieContextClass(DbContextOptions<MovieContextClass> options) : base(options)
+        { }
+        public DbSet<MovieContextClass> Movies { get; set; }
+    }
+}
