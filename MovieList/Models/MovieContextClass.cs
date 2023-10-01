522cfd2f76b5bcadaa38838773248e28e456f05e
Nicholas Woerner <nicholas.woerner@southeasttech.edu>
2023-10-01 18:27:22 -0500

    Steps 8-12

--- MovieList/Models/MovieContextClass.cs
+++ MovieList/Models/MovieContextClass.cs
@@ -1,11 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 
 namespace MovieList.Models
 {
     public class MovieContextClass : DbContext
     {
-        public MovieContextClass(DbContextOptions<MovieContextClass> options) : base(options)
+        public MovieContextClass(DbContextOptions<MovieContextClass> options) 
+            : base(options)
         { }
-        public DbSet<MovieContextClass> Movies { get; set; }
+        public DbSet<MovieClass> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+                modelBuilder.Entity<MovieClass>().HasData(
+                    new MovieClass
+                    {
+                        MovieId = 1,
+                        Name = "Casablanca",
+                        Year = 1942,
+                        Rating = 5
+                    },
+                    new MovieClass
+                    {
+                        MovieId = 2,
+                        Name = "Wonder Woman",
+                        Year = 2017,
+                        Rating = 3
+                    },
+                    new MovieClass
+                    {
+                        MovieId = 3,
+                        Name = "Moonstruck",
+                        Year = 1988,
+                        Rating = 4
+                    }
+                );
+        }
     }
 }
---
+++ MovieList/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using MovieList.Models;
+
+namespace MovieList
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MovieContextClass>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("MovieContext")));
+        }
+    }
+}
